a1e0d781eef0a4e5e267367a266bf842949c0449
ooorange777 <bushibaicai@gmail.com>
2026-02-01 23:13:05 +0800

    add recent tracks

--- src/js/rss.js
+++ src/js/rss.js
@@ -43,3 +43,35 @@ function rss(url, name) {
 for (let i = 0; i < repos.length; i++) {
   rss(repos[i]["url"], repos[i]["name"]);
 }
+
+function lastfmRss(url, name) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      const recentTracks = JSON.parse(xhr.responseText);
+      for (let i = 0; i < 10; i++) {
+        let title = recentTracks.recenttracks.track[i].name;
+        let link = recentTracks.recenttracks.track[i].url;
+        let img = String(recentTracks.recenttracks.track[i].image[1]["#text"]);
+        let titleElement = document.createElement("p");
+        let linkElement = document.createElement("a");
+        let imgElement = document.createElement("img");
+        let itemContainer = document.createElement("div");
+        document.querySelector(name).appendChild(itemContainer);
+        titleElement.textContent = title;
+        linkElement.setAttribute("href", link);
+        linkElement.setAttribute("target", "_blank");
+        imgElement.setAttribute("src", img);
+        imgElement.setAttribute("class", "w-20");
+        titleElement.setAttribute("class", "mt-5");
+        itemContainer.appendChild(linkElement);
+        linkElement.appendChild(imgElement);
+        itemContainer.appendChild(titleElement);
+      }
+    }
+  };
+  xhr.sent();
+}
+const lastfmApi = "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=ooorange777&api_key=e571ab0f73e78879086dd7ef557a4c26&limit=10&format=json";
+lastfmRss(lastfmApi, lastfm);
